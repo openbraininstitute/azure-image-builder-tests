c4b03fc17a91278db2add6aa69e00057cfac15f3
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-05-27 11:47:56 +0200

    Variables

--- image-builder.sh
+++ image-builder.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
 # set variables
-export rg=erik-custom-image-rg
-export location=EastUS2
-export subscription=$(az account show --query id --output tsv)
-export identity_name=erik-image-builder-id
-export image_name=erik-neurodamus
-export run_output_name=erik-run-output
-echo $subscription
+source ./vars.sh
 
-echo "Create resource group"
-az group create --name=${rg} --location=${location}
+EXISTING_RESOURCE_GROUPS=$(az group list | jq -r '.[].name')
+if [[ "${EXISTING_RESOURCE_GROUPS}" == *"${RESOURCE_GROUP_NAME}"* ]]
+then
+    echo "Resource group ${RESOURCE_GROUP_NAME} already exists - skipping create"
+else
+    echo "Create resource group ${RESOURCE_GROUP_NAME}"
+    az group create --name=${RESOURCE_GROUP_NAME} --location=${LOCATION}
+fi
 
 # create identity
-echo "Create managed identity"
-az identity create -g ${rg} -n ${identity_name}
+EXISTING_IDENTITIES=$(az identity list | jq '.[].name')
+if [[ "${EXISTING_IDENTITIES}" == *"${IDENTITY_NAME}"* ]]
+then
+    echo "Identity ${IDENTITY_NAME} already exists - skipping create"
+else
+    echo "Create managed identity"
+    az identity create -g ${RESOURCE_GROUP_NAME} -n ${IDENTITY_NAME}
+fi
+
 # Get the identity ID
-export image_builder_id=$(az identity show -g ${rg} -n ${identity_name} --query clientId -o tsv)
+export image_builder_id=$(az identity show -g ${RESOURCE_GROUP_NAME} -n ${IDENTITY_NAME} --query clientId -o tsv)
 echo "Image builder ID is ${image_builder_id}"
-
-# Get the user identity URI that's needed for the template
-export template_image_builder_id=/subscriptions/${subscription}/resourcegroups/${rg}/providers/Microsoft.ManagedIdentity/userAssignedIdentities/${identity_name}
-
-export image_role_def_name="Azure Image Builder Image Def"
 
 # Update the definition
 cp image_creation_role.json.tpl image_creation_role.json
-sed -i -e "s%<subscriptionID>%${subscription}%g" image_creation_role.json
-sed -i -e "s%<rgName>%${rg}%g" image_creation_role.json
-sed -i -e "s%Azure Image Builder Service Image Creation Role%${image_role_def_name}%g" image_creation_role.json
+sed -i -e "s%<subscriptionID>%${SUBSCRIPTION_ID}%g" image_creation_role.json
+sed -i -e "s%<rgName>%${RESOURCE_GROUP_NAME}%g" image_creation_role.json
+sed -i -e "s%Azure Image Builder Service Image Creation Role%${IMAGE_ROLE_DEF_NAME}%g" image_creation_role.json
 
 # Create role definitions
 az role definition create --role-definition ./image_creation_role.json
@@ -38,16 +40,17 @@ az role definition create --role-definition ./image_creation_role.json
 sleep 5  # sigh
 
 # Grant a role definition to the user-assigned identity
-az role assignment create --assignee ${image_builder_id} --role "${image_role_def_name}" --scope /subscriptions/${subscription}/resourceGroups/${rg}
+az role assignment create --assignee ${image_builder_id} --role "${IMAGE_ROLE_DEF_NAME}" --scope /subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP_NAME}
 
 cp image_template.json.tpl image_template.json
-sed -i -e "s%<subscriptionID>%${subscription}%g" image_template.json
-sed -i -e "s%<rgName>%${rg}%g" image_template.json
-sed -i -e "s%<region>%${location}%g" image_template.json
-sed -i -e "s%<imageName>%${image_name}%g" image_template.json
-sed -i -e "s%<runOutputName>%${run_output_name}%g" image_template.json
-sed -i -e "s%<imgBuilderId>%${template_image_builder_id}%g" image_template.json
+sed -i -e "s%<subscriptionID>%${SUBSCRIPTION_ID}%g" image_template.json
+sed -i -e "s%<rgName>%${RESOURCE_GROUP_NAME}%g" image_template.json
+sed -i -e "s%<region>%${LOCATION}%g" image_template.json
+sed -i -e "s%<imageName>%${IMAGE_NAME}%g" image_template.json
+sed -i -e "s%<runOutputName>%${RUN_OUTPUT_NAME}%g" image_template.json
+sed -i -e "s%<imgBuilderId>%${TEMPLATE_IMAGE_BUILDER_ID}%g" image_template.json
 
-az resource create --resource-group ${rg} --properties '@image_template.json' --is-full-object --resource-type Microsoft.VirtualMachineImages/imageTemplates --name erik-neurodamus
+az resource create --resource-group ${RESOURCE_GROUP_NAME} --properties '@image_template.json' --is-full-object --resource-type Microsoft.VirtualMachineImages/imageTemplates --name erik-neurodamus
 
-az image builder run --name ${image_name} --no-wait --subscription ${subscription} --resource-group ${rg}
+az image builder run --name ${IMAGE_NAME} --no-wait --subscription ${SUBSCRIPTION_ID} --resource-group ${RESOURCE_GROUP_NAME}
+
